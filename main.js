d41beb79df895bea25504bdde8cd0788c3d4e59f
Izaan103 <87347750+Izaan103@users.noreply.github.com>
2022-08-27 13:05:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+  /* not working */
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function saveFilteredImage(){
+  save('filteredImage.png')
+}
